e6c5cc64f4b85eed9e7c724af458c9670ecb35df
frnzska <fadler@babbel.com>
2017-11-09 18:39:51 +0100

    fix date

--- utils/src/iterators_and_ranges.py
+++ utils/src/iterators_and_ranges.py
@@ -9,14 +9,16 @@ def generic_range(start, end, step_size=1):
         yield start
         start = start + step_size
 
-def gen_dates(start_date:dt.datetime, end_date:dt.datetime, days=1):
+
+def gen_dates(start_date:dt.date, end_date:dt.date, days=1):
     """Yields dates, datetime range"""
     while start_date < end_date:
         yield start_date
         start_date = start_date + dt.timedelta(days=days)
 
-def gen_date_sk_range(start:dt.datetime, end:dt.datetime, days=1):
-    """Yields date_sk"""
+
+def gen_date_sk_range(start:dt.date, end:dt.date, days=1):
+    """Yields date_sk range"""
     for date in gen_dates(start, end, days):
         yield date.strftime("%Y%m%d")
 
